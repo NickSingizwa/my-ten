9f00d421f9f079e8076913a27f2bf5b991fbe312
NickSingizwa <singizwanick19@gmail.com>
2024-03-27 22:45:48 +0200

    recognition

---
+++ src/app/company/recognitions/page.tsx
@@ -0,0 +1,49 @@
+import Breadcrump from "@/components/Breadcrump/Breadcrump";
+import HeadingAndParagraphs from "@/components/HeadingAndParagraphs/HeadingAndParagraphs";
+import Landing from "@/components/Landing/Landing";
+import React from "react";
+import Image from "next/image";
+
+const Recognitions = () => {
+  return (
+    <div className="relative mb-8">
+      <Landing title="RECOGNITIONS" imageUrl="https://www.tenaquip.com/tenaquip/images/banner/pages/recognitions.jpg"/>
+      <div className="px-16 flex flex-wrap">
+        <div className="w-[30%] mt-12 flex flex-col mr-8 space-y-3">
+            <div className="border-b-4 py-6 text-center text-3xl">2021</div>
+            <h2 className="text-gray-600 text-xl font-semibold text-center">TENAQUIP celebrates lucky number 13!</h2>
+            <p className="text-black text-center">
+            13 years of being recognized as one of Canada’s Best managed Companies. Thank you to all of our clients and staff for continuing to make this possible and of course to Deloitte and their sponsors, we are proud to be Platinum members!
+            </p> 
+        </div>
+        
+        <div className="w-[30%] mt-12 flex flex-col mr-8 space-y-3">
+            <div className="border-b-4 py-6 text-center text-3xl">2021</div>
+            <h2 className="text-gray-600 text-xl font-semibold text-center">TENAQUIP celebrates lucky number 13!</h2>
+            <p className="text-black text-center">
+            13 years of being recognized as one of Canada’s Best managed Companies. Thank you to all of our clients and staff for continuing to make this possible and of course to Deloitte and their sponsors, we are proud to be Platinum members!
+            </p> 
+        </div>
+
+        <div className="w-[30%] mt-12 flex flex-col mr-8 space-y-3">
+            <div className="border-b-4 py-6 text-center text-3xl">2021</div>
+            <h2 className="text-gray-600 text-xl font-semibold text-center">TENAQUIP celebrates lucky number 13!</h2>
+            <p className="text-black text-center">
+            13 years of being recognized as one of Canada’s Best managed Companies. Thank you to all of our clients and staff for continuing to make this possible and of course to Deloitte and their sponsors, we are proud to be Platinum members!
+            </p> 
+        </div>
+
+        <div className="w-[30%] mt-12 flex flex-col mr-8 space-y-3">
+            <div className="border-b-4 py-6 text-center text-3xl">2021</div>
+            <h2 className="text-gray-600 text-xl font-semibold text-center">TENAQUIP celebrates lucky number 13!</h2>
+            <p className="text-black text-center">
+            13 years of being recognized as one of Canada’s Best managed Companies. Thank you to all of our clients and staff for continuing to make this possible and of course to Deloitte and their sponsors, we are proud to be Platinum members!
+            </p> 
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
+export default Recognitions;
